61529edd2a41e2a0b21642b3d28d7605c2593cf2
DAM1 <DAM1@PC313>
2014-05-26 09:29:45 +0200

    Se implemanta Volador en la clase Pájaro con todos sus métodos.

--- src/com/teide/helipuerto/Pajaro.java
+++ src/com/teide/helipuerto/Pajaro.java
@@ -10,6 +10,16 @@ package com.teide.helipuerto;
  *
  * @author DAM1
  */
-public class Pajaro {
+public class Pajaro implements Volador{
+
+    @Override
+    public String despegar() {
+        return "El pájaro alza el vuelo";
+    }
+
+    @Override
+    public String aterrizar(int metros) {
+       return "El pájaro ha aterrizado ¡y está venga pio pio... y venga y venga a piar!";
+    }
     
 }
